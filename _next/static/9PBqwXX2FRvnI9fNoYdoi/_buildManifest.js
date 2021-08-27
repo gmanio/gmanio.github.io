aa2390ad8c0fed42ffcd5eef650e684967166312
jimanpark <im@gman.io>
2021-08-27 16:55:29 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/9PBqwXX2FRvnI9fNoYdoi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/182-690ecf51480c2f43629b.js","static/chunks/pages/index-2752a53b5b2b008e7a86.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
