afdeeb6e8ac91541a7e6c6f546c9c0df6d5cdd43
jimanpark <im@gman.io>
2021-09-14 01:20:42 +0900

    [feature] modify api

--- src/pages/api/lh.ts
+++ src/pages/api/lh.ts
@@ -1,7 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-const puppeteer = require("puppeteer");
-const $ = require("cheerio");
+import puppeteer from "puppeteer";
+import cheerio from "cheerio";
+import { ElementHandle } from "puppeteer";
 
 type Data = {
   error: boolean;
@@ -19,16 +19,19 @@ export default async function handler(
 ) {
   try {
     const { pblancId } = req.query;
+
     const browser = await puppeteer.launch({
       headless: true,
-      devtools: true,
+      devtools: false,
     });
+
     const page = await browser.newPage();
     const urlParams = new URLSearchParams({ pblancId: pblancId.toString() });
     await page.goto(`${baseUrl}?${urlParams.toString()}`);
-    
+
     const content = await page.content();
-    const body = $.load(content);
+    const body = cheerio.load(content);
+
     const type = body(".viewTop > span").text();
     const title = body(".viewTop > em").text();
     const targets = body(".basicInfo > .info .ds")
@@ -36,33 +39,43 @@ export default async function handler(
       .map((el) => body(el).text());
 
     let houses = [];
-    const list = await page.$$("#hsmpNmUl>li");
+
+    const list: Array<ElementHandle<any>> = await page.$$("#hsmpNmUl>li");
 
     for (const node of Array.from(list)) {
       await node.click();
-      const address = await page.$eval("#fullAdres", (el) => el.innerText);
 
-      const totalNumber = await page.$eval("#lttotHoCo", (el) => el.innerText);
-      const heatType = await page.$eval("#heatMthdNm", (el) => el.innerText);
+      const name = await page.$eval("#hsmpNmUl>.on", (el: any) => el.innerText);
+      const address = await page.$eval("#fullAdres", (el: any) =>
+        el.firstChild.nodeValue.trim()
+      );
+      const totalNumber = await page.$eval("#lttotHoCo", (el: any) =>
+        el.innerText.trim()
+      );
+      const heatType = await page.$eval(
+        "#heatMthdNm",
+        (el: any) => el.innerText
+      );
       const entranceYear = await page.$eval(
         "#mvnPrearngeYear",
-        (el) => el.innerText
+        (el: any) => el.innerText
       );
-      const entranceMonth = await page.$eval(
-        "#mvnPrearngeMt",
-        (el) => el.innerText
+      const entranceMonth = await page.$eval("#mvnPrearngeMt", (el: any) =>
+        el.innerText.trim()
       );
-      const scale = await page.$eval("#dongCo", (el) => el.innerText);
-
-      const infos = await page.$eval("#partclrMatter", (el) => el.innerText);
-
-      const noticeTitle = await page.$eval(
+      const scale = await page.$eval("#dongCo", (el: any) => el.innerText);
+      const infos = await page.$eval(
+        "#partclrMatter",
+        (el: any) => el.innerText
+      );
+      const notice = await page.$eval(
         ".danjiWrap .table_type2 td > a",
-        (el) => el.innerText
-      );
-      const noticeDownload = await page.$eval(
-        ".danjiWrap .table_type2 td > a",
-        (el) => el.href
+        (el: any) => {
+          return {
+            noticeTitle: el.innerText,
+            noticeDownload: el.href,
+          };
+        }
       );
 
       const supplyInfos = await page.$eval("#suplyTableBody", (el) => {
@@ -70,33 +83,98 @@ export default async function handler(
         const items = Array.from(rows).map((row) => {
           const children = row.children;
           return {
-            type: children[0].innerText,
-            exclusive: children[1].innerText,
-            totalNumber: children[2].innerText,
-            priorityNumber: children[3].innerText,
-            normalNumber: children[4].innerText,
-            totalAmount: children[4].innerText,
-            depositAmount: children[5].innerText,
-            middleAmount: children[6].innerText,
-            remainAmount: children[7].innerText,
-            monthlyAmount: children[8].innerText,
+            type: children[0].textContent,
+            exclusive: children[1].textContent,
+            totalNumber: children[2].textContent,
+            priorityNumber: children[3].textContent,
+            normalNumber: children[4].textContent,
+            totalAmount: children[4].textContent,
+            depositAmount: children[5].textContent,
+            middleAmount: children[6].textContent,
+            remainAmount: children[7].textContent,
+            monthlyAmount: children[8].textContent,
           };
         });
         return items;
       });
 
       houses.push({
+        name,
         address,
         totalNumber,
         heatType,
         entranceYear,
         entranceMonth,
         scale,
-        notice: { noticeTitle, noticeDownload },
+        notice,
         infos,
         supplyInfos,
       });
     }
+
+    const plansInfo = await page.$eval(".schInfo .table_type2", (el: any) => {
+      let plansInfo = {};
+      let supplyInfos: any = [];
+
+      el.querySelectorAll("tr").forEach((row: any) => {
+        if (row.querySelector("th").innerText === "일반공급") {
+          supplyInfos.push({
+            supplyType: "일반공급",
+            priority:
+              row.querySelectorAll("th").length > 2
+                ? row.querySelectorAll("th")[1].innerText
+                : "1순위",
+            conditions: Array.from(row.querySelectorAll("td dt")).map(
+              (dt: any) => {
+                return {
+                  title: dt.innerText,
+                  description: dt.nextElementSibling.innerText,
+                };
+              }
+            ),
+          });
+        }
+
+        if (row.querySelector("th").innerText === "2,3 순위") {
+          supplyInfos.push({
+            supplyType: "일반공급",
+            priority: row.querySelector("th").innerText,
+            conditions: Array.from(row.querySelectorAll("td dt")).map(
+              (dt: any) => {
+                return {
+                  title: dt.innerText,
+                  description: dt.nextElementSibling.innerText,
+                };
+              }
+            ),
+          });
+        }
+
+        if (row.querySelector("th").innerText === "모집공고일") {
+          Object.assign(plansInfo, {
+            planDate: row.querySelector("td").innerText,
+          });
+        }
+
+        if (row.querySelector("th").innerText === "당첨자 발표일") {
+          Object.assign(plansInfo, {
+            lotteryDate: row.querySelector("td").innerText,
+          });
+        }
+
+        if (row.querySelector("th").innerText === "일정관련 안내사항") {
+          Object.assign(plansInfo, {
+            planNoticeInfo: row.querySelector("td").innerText,
+          });
+        }
+      });
+
+      if (supplyInfos.length > 0) {
+        Object.assign(plansInfo, { supplyInfos: [...supplyInfos] });
+      }
+
+      return plansInfo;
+    });
 
     await browser.close();
 
@@ -104,7 +182,7 @@ export default async function handler(
       statusCode: 200,
       error: false,
       errorMessage: "",
-      result: { targets, type, title, houses },
+      result: { targets, type, title, houses, plansInfo },
     });
   } catch (err: any) {
     res.status(500).json({
